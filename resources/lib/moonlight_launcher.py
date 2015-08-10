02188d9fc642f8e52b4349fdcb6d50dfc5097cd6
msfabxda <fabian.s90s@googlemail.com>
2015-08-10 22:25:48 +0200

    Update moonlight_launcher.py

--- resources/lib/moonlight_launcher.py
+++ resources/lib/moonlight_launcher.py
@@ -25,8 +25,6 @@ class Launch():
         isCompatible = False
         availableSys = ['win32', 'darwin', 'linux']
         currentSys   = sys.platform
-
-        java      = __settings__("java_path").replace("\\", "\\\\")
         moonlight = __settings__("moonlight_path").replace("\\", "\\\\")
         ip        = __settings__("host_ip")
         reso      = __settings__("resolution").replace("p","")
@@ -46,9 +44,9 @@ class Launch():
                 isCompatible = True
 
         if isCompatible:
-            if self.checkSettings(__settings__) and self.checkJava(currentSys):
-                if self.isPaired(java, moonlight, args_dash, ip):
-                    self.startProcess(java, moonlight, args_dash, ip, reso, fps, fullscrn, strm_cmd)
+            if self.checkSettings(__settings__):
+                if self.isPaired(moonlight, args_dash, ip):
+                    self.startProcess( moonlight, args_dash, ip, reso, fps, fullscrn, strm_cmd)
                 else:
                     self.fail("Connection error - Can't pair to host")
         else:
@@ -66,48 +64,28 @@ class Launch():
             self.fail("Please set up the host IP in settings !")
             return False
 
-        elif len(settings("java_path")) < 1:
-            self.fail("Please set up the Java path in settings !")
-            return False
-
         return True
 
 
 
-    def checkJava(self, system):
 
-        p = subprocess.Popen('"'+__settings__('java_path')+'" -version', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        if 'java version' in p.stdout.readlines()[0]:
-            return True
-        else:
-            return False
-
-
-
-    def stream(self, java, moonlight, ip, reso, fps, fullscreen, stream):
+    def stream(self,moonlight, ip, reso, fps, fullscreen, stream):
 
         __dialog__.notification(__name__, "Streaming from host...", xbmcgui.NOTIFICATION_INFO, 5000)
 
-        streaming = subprocess.Popen('"'+java+'" -jar "'+moonlight+'" '+stream+' '+ip+' -'+reso+' -'+fps+fullscreen, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        streaming = subprocess.Popen('"'+moonlight+' '+stream+' '+ip+' -'+reso+' -'+fps+fullscreen, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         sys.stdout.flush()
         for line in iter(streaming.stdout.readline, b''):
             sys.stdout.flush()
-            if "java.net.ConnectException" in line:
-                self.fail(line)
-                streaming.kill()
-
 
     def isPaired(self, java, moonlight, dash, ip):
 
         __dialog__.notification(__name__, "Pairing to host...", xbmcgui.NOTIFICATION_INFO, 5000)
 
-        pairing = subprocess.Popen('"'+java+'" -jar "'+moonlight+'" '+dash+'pair '+ip, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        pairing = subprocess.Popen('"'+moonlight+' '+dash+'pair '+ip, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         sys.stdout.flush()
         for line in iter(pairing.stdout.readline, b''):
             sys.stdout.flush()
-            if "java.net.ConnectException" in line:
-                pairing.kill()
-                return False
         if pairing.wait() == 1:
             return True
         else: 
